be5a74305d2a43c6c685f4a3f65add15f87a4bd7
SergiFuster <sergifusterdura@gmail.com>
2025-03-02 00:44:21 +0100

    feat&fix(enums): Enums support and lists fix

--- src/parsdantic/parser.py
+++ src/parsdantic/parser.py
@@ -1,9 +1,11 @@
+from enum import Enum
 from typing import Any, List
 
 import jsonref
 from pydantic import BaseModel, create_model
 
 Types = {"integer": int, "string": str, "boolean": bool}
+
 
 def parse(obj: dict[str, Any]) -> type[BaseModel]:
     """Convert JSON Schema to Pydantic model
@@ -14,17 +16,24 @@ def parse(obj: dict[str, Any]) -> type[BaseModel]:
     Returns:
         type[BaseModel]: Resulting Pydantic model
     """
-    
+
     def _parse_list(title: str, obj: dict[str, dict[str, dict]]) -> type[BaseModel]:
-        match obj.get("type"):
+        match obj.get("type", "enum"):
             case "object":
                 return _parse_object(obj["title"], obj["properties"])
             case "array":
-                return List[_parse_list(obj["title"], obj["items"])]
+                return List[_parse_list(title, obj["items"])]
             case _:
-                return Types[obj["type"]]
-            
-    def _parse_object(title: str, obj: dict[str, dict[str, dict]], res={}) -> type[BaseModel]:
+                if obj.get("enum"):
+                    return Enum(
+                        obj["title"], {item.upper(): item for item in obj["enum"]}
+                    )
+                else:
+                    return Types[obj["type"]]
+
+    def _parse_object(
+        title: str, obj: dict[str, dict[str, dict]], res={}
+    ) -> type[BaseModel]:
         """Convert JSON Schema to Pydantic model
 
         Args:
@@ -36,15 +45,26 @@ def parse(obj: dict[str, Any]) -> type[BaseModel]:
             type[BaseModel]: _description_
         """
         for k, v in obj.items():
-            match v.get("type"):
+            match v.get("type", "enum"):
                 case "object":
-                    res = res | {k : (_parse_object(v["title"], v["properties"]), None)}
+                    res = res | {k: (_parse_object(v["title"], v["properties"]), None)}
                 case "array":
-                    res = res | {k : (List[_parse_list(v["title"], v["items"])], None)}
+                    res = res | {k: (List[_parse_list(v["title"], v["items"])], None)}
                 case _:
-                    res = res | {k: (Types[v["type"]], None)}
+                    if v.get("enum"):
+                        res = res | {
+                            k: (
+                                Enum(
+                                    v["title"],
+                                    {item.upper(): item for item in v.get("enum")},
+                                ),
+                                None,
+                            )
+                        }
+                    else:
+                        res = res | {k: (Types[v["type"]], None)}
 
         return create_model(title, **res)
-    
+
     obj = jsonref.replace_refs(obj)
     return _parse_object(obj["title"], obj["properties"])
